c079a707bdc240139a7680a80b3045ddbf1dc0e4
Trevor Hodde <thodde@worcester.edu>
2013-04-03 15:35:12 -0700

    Adding comments

--- client/AddressUtility.c
+++ client/AddressUtility.c
@@ -1,15 +1,15 @@
-/*
-	File Name:		AddressUtility.c
-	Author:			Trevor Hodde
-*/
+/**
+ * File Name: AddressUtility.c
+ * Author:    Trevor Hodde
+ */
 
 #include <arpa/inet.h>
 #include "Client.h"
 
 void printSocketAddress(const struct sockaddr *address, FILE *stream){
-	void 		*numeric_address;					//pointer to binary address
-	char 		address_buffer[INET6_ADDRSTRLEN];	//address data
-	in_port_t 	port;								//port number
+	void 		*numeric_address;
+	char 		address_buffer[INET6_ADDRSTRLEN];
+	in_port_t 	port;
 	
 	//Test for address and stream
 	if (address == NULL || stream == NULL){
@@ -18,18 +18,16 @@ void printSocketAddress(const struct sockaddr *address, FILE *stream){
 	
 	//Set pointer to address based on address family
 	switch (address->sa_family){
-		//The type of address is ipv4
 		case AF_INET:
 			numeric_address = &((struct sockaddr_in*)address)->sin_addr;
 			port = ntohs(((struct sockaddr_in*)address)->sin_port);
 			break;
-		//Invalid address
 		default:
+			//Invalid address
 			fputs("[unknown type]", stream);
 			return;
 	}
 	
-	//Convert binary to printable address
 	if (inet_ntop(address->sa_family, numeric_address, address_buffer, sizeof(address_buffer)) == NULL){
 		fputs("[invalid address]", stream);
 	}
